4f3fec274da578aea27320e5edb82b984545e507
Teunis van Wijngaarden <info@teuniz.nl>
2018-05-29 10:26:58 +0200

    modulesofschoolclass tree and table

--- src/main/html/js/ModulesOfSchoolclassDisplay.js
+++ src/main/html/js/ModulesOfSchoolclassDisplay.js
@@ -1,15 +1,15 @@
 function ModulesOfSchoolclassDisplay() {	
 	// GWT vars
 	
+	this.openNodes = [];
+	this.activeModuleList = [];
 	
 	// Forms 
 	this.selectForm = document.forms["modulesOfSchoolclassDisplaySelect"];
 	this.settingsForm = document.forms["modulesOfSchoolclassDisplaySettings"];
 	this.searchForm = document.forms["modulesOfSchoolclassDisplaySearch"];
 	
-	
-	// Buttons 
-	
+			
 	// jQuery objects
 	this.$panel = jQuery("#modulesOfSchoolclassDisplay");
 	this.$treeWrapper = jQuery("#modulesOfSchoolclassDisplayTreeWrapper");
@@ -24,7 +24,6 @@ function ModulesOfSchoolclassDisplay() {
 	
 	this.$reloadButton = $(this.searchForm.elements["reload"]);
 	
-
 	// Bind handlers
 	this.$settingsForm.on('submit', $.proxy(this.submitSettings,this));	
 	this.$searchForm.on('submit', $.proxy(this.submitSearch,this));	
@@ -72,38 +71,10 @@ ModulesOfSchoolclassDisplay.prototype.reloadTree = function() {
 	this.$tree.find("li").removeClass("open");	
 }
 
-/*
- * VIEW FUNCTIONS
- * Map to java implementation
- */
-
-ModulesOfSchoolclassDisplay.prototype.clear = function () {
-	console.log("clear");
-}
-
-ModulesOfSchoolclassDisplay.prototype.init = function () {
-	console.log("init");	
-}
-
-ModulesOfSchoolclassDisplay.prototype.setEmptyTableMessageModules = function () {
-	console.log("setEmptyTableMessageModules");
-}
-ModulesOfSchoolclassDisplay.prototype.setLoadingTableMessageModules = function () {
-	console.log("setLoadingTableMessageModules");
-}
-ModulesOfSchoolclassDisplay.prototype.setEmptyTableMessageSelected = function () {
-	console.log("setEmptyTableMessageSelected");
-}
-ModulesOfSchoolclassDisplay.prototype.setLoadingTableMessageSelected = function () {
-	console.log("setLoadingTableMessageSelected");
-}
-
-
-ModulesOfSchoolclassDisplay.prototype.updateTable = function(json) {
+ModulesOfSchoolclassDisplay.prototype.updateTable = function() {
 	console.log("UPDATE!");
-	console.log(json);	
 	
-	var modules = json;
+	var modules = this.activeModuleList;
 	
 	this.$selectTableBody.html("");
 	
@@ -114,19 +85,47 @@ ModulesOfSchoolclassDisplay.prototype.updateTable = function(json) {
 		$row = this.$selectRow.clone();
 		$row.prop('tabindex', i);
 		$row.find("#modulesOfSchoolclassDisplaySelectId").val( id ).removeAttr("id");
-		$row.find("#modulesOfSchoolclassDisplaySelectName").html( el ).removeAttr("id");
-
+		$row.find("#modulesOfSchoolclassDisplaySelectName").html( el.name ).removeAttr("id");
+		
 		$row.find("input[type='checkbox'],input[type='radio']").each( function() {
 			this.value = id;
 		});
 
 		$row.on('click keypress', $.proxy(this.clickSelectRow, this));
+		
 		this.$selectTableBody.append($row);
 		i++;
 	}
-	
+	return;
+}
+
+/*
+ * VIEW FUNCTIONS
+ * Map to java implementation
+ */
+
+ModulesOfSchoolclassDisplay.prototype.clear = function () {
+	console.log("clear");
+}
+
+ModulesOfSchoolclassDisplay.prototype.init = function () {
+	console.log("init");
 	Helpers.stretchHeight( [this.$treeWrapper, this.$selectTableBody] );
 }
+
+ModulesOfSchoolclassDisplay.prototype.setEmptyTableMessageModules = function () {
+	console.log("setEmptyTableMessageModules");
+}
+ModulesOfSchoolclassDisplay.prototype.setLoadingTableMessageModules = function () {
+	console.log("setLoadingTableMessageModules");
+}
+ModulesOfSchoolclassDisplay.prototype.setEmptyTableMessageSelected = function () {
+	//this.$selectTableBody.html('<tr class="empty"><td>Geen modules gevonden</td></tr>');
+}
+ModulesOfSchoolclassDisplay.prototype.setLoadingTableMessageSelected = function () {
+	this.$selectTableBody.html('<tr class="loading"><td>Laden...</td></tr>');
+}
+
 ModulesOfSchoolclassDisplay.prototype.setTree = function(json) {
 	console.log("SET TREE");
 	console.log(json);	
@@ -137,17 +136,22 @@ ModulesOfSchoolclassDisplay.prototype.setTree = function(json) {
 	
 	result = '<ul id ="modulesOfSchoolclassDisplayTree" class="tree">'+result+'</li>';
 	$result = $(result);
-	$result.find("li.hasSub a").click(Helpers.clickTreeNode);
+	$result.find("li.hasSub a").on('click', $.proxy(this.clickTreeNode, this)); //click(Helpers.clickTreeNode);
 	$result.find("input").on('change', $.proxy(this.toggleTreeCheckbox, this));
 	
 	this.$tree = $result;
 	this.$treeWrapper.html("");
 	this.$treeWrapper.append($result);
-	Helpers.stretchHeight( [this.$treeWrapper, this.$selectTableBody] );
+	
+	console.log(this.activeModuleList);
+	this.updateTable();
+	
+	
 }
 
+// Helper:
 ModulesOfSchoolclassDisplay.prototype.recursiveTreeBuilder = function(tree, depth = 0, checkboxId = 0) {
-	var result, subtree, liClass, aClass, checkboxId, checked;
+	var result, subtree, liClass, aClass, checkboxId, checked, checkboxClass;
 	
 	if (!tree) return; //sometimes it is undefined
 	
@@ -164,6 +168,7 @@ ModulesOfSchoolclassDisplay.prototype.recursiveTreeBuilder = function(tree, dept
 		checkboxId += "" + depth + i
 		liClass = "";
 		aClass = "";
+		checkboxClass = "";
 		
 		if (tree[id].data.course.withChildren == true) {
 			subtree = this.recursiveTreeBuilder(tree[id].children, depth + 1, checkboxId);
@@ -173,16 +178,27 @@ ModulesOfSchoolclassDisplay.prototype.recursiveTreeBuilder = function(tree, dept
 			aClass="set";
 		}
 		
-		if (tree[id].data.classcourse != null) checked = 'checked="checked"';
-		else checked = "";
+		if ( this.openNodes != null && this.openNodes.hasOwnProperty(id)  ) {
+			if (this.openNodes[id] === true) liClass += " open";
+		}
+
+		if (tree[id].data.classCourse != null) {
+			if (tree[id].data.classCourse.viewState == "invisible") checkboxClass = "previouslyChecked";
+			 else {
+				 checked = 'checked="checked"';
+				 this.activeModuleList[id] = tree[id].data.course;
+			 }
+		} else {
+			checked = "";
+		}		
 		
-		result += '<li class="'+liClass+'">';
+		result += '<li class="'+liClass+'" data-id="'+id+'">';
 		result += '<a class="icon '+aClass+'">';
 		result += tree[id].data.course.name;		
 		result += '</a>';
 		
-		result += '<div class="checkbox">';
-		result += '<input type="checkbox" name="module" id="'+checkboxId+'" value="'+tree[id].data.course.id.idString+'" '+checked+'>';
+		result += '<div class="checkbox '+checkboxClass+'">';
+		result += '<input type="checkbox" name="module" id="'+checkboxId+'" value="'+id+'" '+checked+'>'; //tree[id].data.course.id.idString
 		result += '<label class="icon" for="'+checkboxId+'"></label>';
 		result += '</div>';
 		
@@ -207,16 +223,18 @@ ModulesOfSchoolclassDisplay.prototype.recursiveTreeBuilder = function(tree, dept
  */
 
 ModulesOfSchoolclassDisplay.prototype.attachItem = function(id) {
+	console.log("attach "+id);
 	app.getPresenterFactory().modulesOfSchoolclassPresenter.attachItemToSchoolClass(id);
 }
 
 ModulesOfSchoolclassDisplay.prototype.detachItem = function(id) {
+	console.log("detach "+id);
 	app.getPresenterFactory().modulesOfSchoolclassPresenter.detachItemFromSchoolClass(id);
 }
 
-ModulesOfSchoolclassDisplay.prototype.detachItem = function(id) {
-	app.getPresenterFactory().modulesOfSchoolclassPresenter.detachItemFromSchoolClass(id);
-}
+// ModulesOfSchoolclassDisplay.prototype.detachItem = function(id) {
+// 	app.getPresenterFactory().modulesOfSchoolclassPresenter.detachItemFromSchoolClass(id);
+// }
 
 ModulesOfSchoolclassDisplay.prototype.setModuleSettings = function() {
 	//String key, String typeString, String fromData, String toData, String accessKey
@@ -230,12 +248,24 @@ ModulesOfSchoolclassDisplay.prototype.setModuleSettings = function() {
 }
 
 /*
- * EVENT HANDLERS - tree checkboxes
+ * EVENT HANDLERS - tree 
  */
 
 ModulesOfSchoolclassDisplay.prototype.toggleTreeCheckbox = function(event) {
 	if (event.target.checked) this.attachItem(event.target.value);
 	else this.detachItem(event.target.value);			
+}
+
+ModulesOfSchoolclassDisplay.prototype.clickTreeNode = function(event) {
+	var $el = $(event.target).parent();
+	console.log($el);
+	if ($el.hasClass("open")) {
+		$el.removeClass("open");
+		this.openNodes[$el.data("id")] = false;
+	} else {
+		$el.addClass("open");
+		this.openNodes[$el.data("id")] = true;
+	}
 }
 
 
--- src/main/html/js/helpers.js
+++ src/main/html/js/helpers.js
@@ -49,7 +49,7 @@ Helpers.toggleHelpSection = function() {
 }
 
 Helpers.stretchHeight = function(elements) {
-	console.log("STRETCH!");
+	//console.log("STRETCH!");
 	if (elements.length < 1) return;
 	
 	subpanel = elements[0].closest('.subpanel');
@@ -57,9 +57,9 @@ Helpers.stretchHeight = function(elements) {
 	bodyHeight = $(document.body).outerHeight();
 	freeSpace = bodyHeight - subpanelHeight;
 	
-	console.log(bodyHeight);
-	console.log(subpanelHeight);
-	console.log(freeSpace);
+	//console.log(bodyHeight);
+	//console.log(subpanelHeight);
+	//console.log(freeSpace);
 	
 	for(i=0; i<elements.length; i++) {
 		newHeight = elements[i].height() + freeSpace;
@@ -77,7 +77,3 @@ Helpers.stretchIframeHeight = function(iframe) {
 	iframe.outerHeight(subpanelHeight+"px");
 }
 
-Helpers.clickTreeNode = function() {
-	console.log("Help");
-	$(this).parent().toggleClass("open");
-}
