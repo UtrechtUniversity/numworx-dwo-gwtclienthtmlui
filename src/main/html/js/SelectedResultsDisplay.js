a2b06f7384aba7125b0b50262f77c04bcabe1165
Teunis van Wijngaarden <info@teuniz.nl>
2018-06-12 17:12:38 +0200

    selected results new level

--- src/main/html/js/SelectedResultsDisplay.js
+++ src/main/html/js/SelectedResultsDisplay.js
@@ -49,46 +49,73 @@ SelectedResultsDisplay.prototype.plotMatrix = function (matrix) {
 		$rowCell = null,
 		$value = null;
 	
+	// BUILD HEADER
 	for (var i = 1; i < matrix[0].length; i++) {
+				// Header names
 		$headerName = this.$selectedResultsColumnHeaderName.clone();
-		$headerName.html("<span>" + matrix[0][i].label + "</span>");
+		$value = $("<span>" + matrix[0][i].label + "</span>");
+		$headerName.html("").append($value);		
+		if (matrix[0][i].callback) {
+			$value.on('click', $.proxy(matrix[0][i].callback, this));
+		}
+		if (matrix[0][i].linkLabel && matrix[0][i].linkCallback) {
+			$link = $('<a href="javascript:void(0);">'+matrix[0][i].linkLabel+'</a>');
+			$value.append($link);
+			$link.on('click', $.proxy(matrix[0][i].linkCallback, this));
+		}
+		$value.hover($.proxy(this.hoverColumnHeader, this));		
 		$theadRow2.append($headerName);
+		
+		// Sort buttons
+		$headerSorting = this.$selectedResultsColumnHeaderSorting.clone();
+		$theadRow3.append($headerSorting);		
+		$theadRow3.find(".sortButton").click(Helpers.clickSortButton);
 	}
 	
+	// BUILD BODY
 	for (var i = 1; i < matrix.length; i++) {
 		
 		$row = this.$selectedResultsRow.clone();
 		
+		// row first column
 		$tableRowHeader = this.$selectedResultsRowHeader.clone();
-		console.log($tableRowHeader);
 		$tableRowHeader.html("");
-		$tableRowHeader.append("<span>" + matrix[i][0].label + "</span>");
+		$value = $("<span>" + matrix[i][0].label + "</span>");
+		$value.attr("data-sortvalue", matrix[i][0].label );		
+		$tableRowHeader.append($value);
 		$row.append($tableRowHeader);
 		
+		// row rest of the columns
 		for (var j = 1; j < matrix[i].length; j++) {
 			$rowCell = this.$selectedResultsRowCell.clone();
 			$rowCell.html("");
 			if (matrix[i][j].label != "") {
 				$value = $("<a class=\"resultIndicator\">" + matrix[i][j].label +"</a>");
 				$value.attr("data-score", matrix[i][j].label );
+				$value.attr("data-sortvalue", matrix[i][j].label );
 				Helpers.setResultIndicatorColor($value);
 				
 				if (matrix[i][j].callback) {
 					$value.on('click', $.proxy(matrix[i][j].callback, this));
 				}
-				
+
 				$rowCell.append($value);
 			} else {
 				$rowCell.html("&nbsp;");
 			}
+			console.log($rowCell);
 			$row.append($rowCell);
 		} 		
 		$tbody.append($row);
 	}
 	
+	// Sizing
 	if (matrix[0].length > 10) this.$selectResultsTableWrap.addClass("overflow");
 	else this.$selectResultsTableWrap.removeClass("overflow");
 	
+	this.$selectResultsTableWrap.removeClass(function (index, className) {
+    	return (className.match (/(^|\s)size-\S+/g) || []).join(' ');
+	});
 	this.$selectResultsTableWrap.addClass("size-"+matrix[0].length);
 	
 	this.$selectResultsTableWrap.html("");
@@ -109,6 +136,9 @@ SelectedResultsDisplay.prototype.buildMatrixModulesStudentsForClass = function()
 	for (var amId in activeModules) {
 		matrix[0][j] = {};
 		matrix[0][j].label = modules[ activeModules[amId] ].label;
+		matrix[0][j].callback = function(event) { event.stopPropagation(); event.preventDefault();	this.activitiesStudents( modules[ activeModules[amId] ] ); };
+		matrix[0][j].linkLabel = "activiteiten";
+		matrix[0][j].linkCallback = function() {  event.stopPropagation(); event.preventDefault();	this.activitiesStudents( modules[ activeModules[amId] ] ); };
 		j++;
 	}
 	
@@ -133,7 +163,7 @@ SelectedResultsDisplay.prototype.buildMatrixModulesStudentsForClass = function()
 
 SelectedResultsDisplay.prototype.buildMatrixActivitiesStudentInModule = function(module, studentId) {
 	var matrix = [], i = 1, j = 1;
-	var students = this.resultState.studentsTree.children[ this.resultState.activeSchoolClass ].children
+	var students = this.resultState.studentsTree.children[ this.resultState.activeSchoolClass ].children;
 	
 	matrix[0] = [];
 	matrix[0][0] = "colsname";
@@ -169,7 +199,48 @@ SelectedResultsDisplay.prototype.buildMatrixActivitiesStudentInModule = function
 		j++;
 	}
 	
-	return matrix
+	return matrix;
+}
+
+SelectedResultsDisplay.prototype.buildMatrixActivitiesStudentsInModule = function(module) {
+	var matrix = [], i = 1, j = 1;
+	var students = this.resultState.studentsTree.children[ this.resultState.activeSchoolClass ].children;
+	
+	matrix[0] = [];
+	matrix[0][0] = "colsname";
+	
+	for (var actId in module.children) {
+		matrix[0][j] = {};
+		matrix[0][j].label = module.children[actId].label;
+		j++;
+	}
+	
+	console.log(module);
+	
+	for (var studentId in students) {
+		matrix[i] = [];
+		matrix[i][0] = {};
+		matrix[i][0].label = students[studentId].givenName + " " + (students[studentId].insertion ? students[studentId].insertion+" ":"")  + students[studentId].familyName;
+		
+		j = 1;
+		for (var actId in module.children) {
+			console.log("i:"+i+" j:"+j);
+			console.log(module.children[actId]);
+			matrix[i][j] = {};
+			matrix[i][j].label = this.computeActivityScoreForStudent(module.children[actId], studentId);
+			matrix[i][j].callback = function() { console.log("callback"); };
+			j++;
+		}		
+		
+		// if (matrix[i][j] == undefined) {
+// 			matrix[i][j] = {};
+// 			matrix[i][j].label = "";
+// 		}
+		
+		i++;	
+	}
+	
+	return matrix;
 }
 
 
@@ -191,6 +262,19 @@ SelectedResultsDisplay.prototype.computeModuleScoreForStudent = function(module,
 	return Math.round(total / totalCount);
 }
 
+SelectedResultsDisplay.prototype.computeActivityScoreForStudent = function(activity, studentId) {
+	var total = 0, totalCount = 0, scoreSet = false;
+	
+	for (var scoId in activity.children) { // Loop over activities
+		if (activity.children[scoId]["user-id"] == studentId) { // Select student
+			scoreSet = true;
+			total += parseInt(activity.children[scoId].sumScore); // Sum of scores
+		}	
+		totalCount++;
+	}
+	if (!scoreSet) return "";
+	return Math.round(total / totalCount);
+}
 
 /*
  * VIEWS
@@ -202,12 +286,16 @@ SelectedResultsDisplay.prototype.modulesStudents = function() {
 }
 
 SelectedResultsDisplay.prototype.activitiesStudent = function(module, studentId) {
-	console.log("show activities in module for student")
-	console.log(module);
-	console.log(studentId);
 	var matrix = this.buildMatrixActivitiesStudentInModule(module, studentId);
 	this.plotMatrix(matrix);
 }
+
+SelectedResultsDisplay.prototype.activitiesStudents = function(module) {
+	console.log(module);
+	var matrix = this.buildMatrixActivitiesStudentsInModule(module);
+	this.plotMatrix(matrix);
+}
+
 
 /*
  * VIEW FUNCTIONS
@@ -242,4 +330,14 @@ SelectedResultsDisplay.prototype.updateResultTree = function (resultsTree, stude
  * EVENT HANDLERS
  */
 
-
+SelectedResultsDisplay.prototype.hoverColumnHeader = function(event) {
+	console.log("hover");
+	var $target = $(event.target);
+	if ($target.hasClass('active')) {
+		$target.removeClass('active');
+		$target.find('a').hide();
+	} else {
+		$target.addClass('active');
+		$target.find('a').show();
+	}	
+}
