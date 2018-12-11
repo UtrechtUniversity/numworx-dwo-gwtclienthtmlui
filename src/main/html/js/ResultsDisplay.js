ca28b25a15c01ebdf90ec268aad6e83462befa32
peterboon <peterboon@192.168.2.1>
2018-12-11 11:17:38 +0100

    met activeschoolclass en activecourses[]

--- src/main/html/js/ResultsDisplay.js
+++ src/main/html/js/ResultsDisplay.js
@@ -214,7 +214,7 @@ ResultsDisplay.prototype.setLoadingTableMessage = function () {
  */
 
 ResultsDisplay.prototype.showSelectedResults = function() {	
-	app.getPresenterFactory().getResultsPresenter().showSelectedResults( this.resultState );
+	app.getPresenterFactory().getResultsPresenter().showSelectedResults( this.resultState, this.resultState.activeSchoolClass, this.resultState.activeCourses );
 }
 
 
