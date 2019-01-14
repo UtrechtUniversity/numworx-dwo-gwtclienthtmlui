48799649955ac465fdf73ba34195761dbb305c3c
Wim van Velthoven <w.p.g.vanvelthoven@uu.nl>
2019-01-14 07:48:22 +0100

    ANONYMOUS komt ook voor als class

--- src/main/html/js/MainDisplay.js
+++ src/main/html/js/MainDisplay.js
@@ -147,7 +147,10 @@ MainDisplay.prototype.setSchoolName = function (schoolName) {
 	this.$accountMenuSchoolName.html(schoolName);
 };
 MainDisplay.prototype.setUserRole = function (role) {
-	this.$body.removeClass("TEACHER"); this.$body.removeClass("SCHOOLADMIN"); this.$body.addClass(role);
+	this.$body.removeClass("ANONYMOUS");
+	this.$body.removeClass("TEACHER");
+	this.$body.removeClass("SCHOOLADMIN");
+	this.$body.addClass(role);
 	role = app.getTranslator().translate( 'NUM_APP_' + role);
     this.$accountMenuUserRole.html(role);
 	document.title = "Numworx " + role;
