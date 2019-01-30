757307dbbf0d955bab8c0c550019d7c522c6c7a6
peterboon <peterboon@imac-bbg303.local>
2019-01-30 11:47:34 +0100

    IE11 fix

--- src/main/html/js/AccountDisplay.js
+++ src/main/html/js/AccountDisplay.js
@@ -189,7 +189,10 @@ AccountDisplay.prototype.saveSchoolLogins = function(event) {
 }
 
 AccountDisplay.prototype.addSchoolLogin = function(event) {
-	app.getPresenterFactory().getAccountPresenter().addASchoolLogin( this.addSchoolLoginForm.elements["role"].value,
+	var role;
+	role = this.addSchoolLoginForm.elements["role"].value; // Niet in IE11
+	role = document.querySelector('form[name=addSchoolLogin] input[name=role]:checked').value;
+	app.getPresenterFactory().getAccountPresenter().addASchoolLogin( role,
 																this.addSchoolLoginForm.elements["schoolLogin"].value,
 																this.addSchoolLoginForm.elements["schoolCode"].value );	
 }
