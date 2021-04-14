060cd29f3cd191b5af3d323cadaa8233f44f12b8
Wim van Velthoven <w.p.g.vanvelthoven@uu.nl>
2021-04-14 16:30:54 +0200

    https://numworx.atlassian.net/browse/LMS-501
    javascript functies voor presenter

--- src/main/html/js/TeacherStudentModelDisplay.js
+++ src/main/html/js/TeacherStudentModelDisplay.js
@@ -7,6 +7,18 @@
  	this.$treeWrapper = $("#teacherStudentModelTreeWrapper")
  	this.$description = $("#teacherStudentModelDescription")
  
+ 	this.schoolclassForm = document.forms['studentmodelKlas'];
+ 	this.schoolClassSelect = this.schoolclassForm.elements["schoolClass"];
+	this.$schoolClassSelect = $(this.schoolClassSelect);
+	this.$schoolClassSelectOption = this.$schoolClassSelect.find("option").detach();
+ 
+ 	this.studentModelForm = document.forms['teacherStudentModelSelect']
+ 	this.studentModelSelect = this.studentModelForm.elements['modelselect']
+ 	this.$studentModelSelect = $(this.studentModelSelect)
+ 	this.$studentModelSelectOption = this.$studentModelSelect.find("option").detach();
+ 
+ 	this.$studentModelSelect.on('change', $.proxy(
+ 
  	// Init
 	this.$panel.hide();
  }
@@ -33,3 +45,49 @@ TeacherStudentModelDisplay.prototype.clear = function () {
 TeacherStudentModelDisplay.prototype.setHelp = function(url) {
 	if (this.$helpContentIFrame.attr('src') != url) this.$helpContentIFrame.attr('src', url );
 }
+
+
+TeacherStudentModelDisplay.prototype.showSchoolClasses = function(json) {
+	var $option;
+	this.schoolClasses = json;
+	console.log(json);
+	this.$schoolClassSelect.html("");
+	
+	$option = this.$schoolClassSelectOption.clone();		
+	$option.val( "" ).removeAttr("id").html( "" );
+	this.$schoolClassSelect.append($option);
+	
+	for (var id in this.schoolClasses) { 
+		$option = this.$schoolClassSelectOption.clone();		
+		$option.val( id ).removeAttr("id").html( Helpers.htmlEscape(this.schoolClasses[id].schoolClass.schoolClassName) );
+		this.$schoolClassSelect.append($option);
+	}
+}
+
+TeacherStudentModelDisplay.prototype.showModels = function(json) {
+	var $option;
+	this.titles = json;
+	console.log(json);
+	this.$studentModelSelect.html("");
+	$option = this.$schoolClassSelectOption.clone();		
+	$option.val( "" ).removeAttr("id").html( "" );
+	this.$studentModelSelect.append($option);
+	
+	for (var id in this.titles) { 
+		$option = this.$studentModelSelectOption.clone();		
+		$option.val( id ).removeAttr("id").html( Helpers.htmlEscape(this.titles[id]) );
+		this.$studentModelSelect.append($option);
+	}
+}
+
+TeacherStudentModelDisplay.prototype.showTree = function(json) {
+
+}
+
+
+//Events
+
+TeacherStudentModelDisplay.prototype.onModelChange = function(ev) {
+	ev.preventDefault();
+	var id =  this.studentModelSelect.value;
+	app.getPresenterFactory().getStudentModelPresenter().selectModel(id);
