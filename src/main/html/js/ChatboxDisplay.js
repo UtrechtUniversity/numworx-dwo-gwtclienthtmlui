4af07e1a9e9ecdaf176465b87bdcbfb67cadd5fd
Wim van Velthoven <w.p.g.vanvelthoven@uu.nl>
2022-07-20 15:43:31 +0200

    Chat functie, via converse en verify.jsp

--- src/main/html/js/ChatboxDisplay.js
+++ src/main/html/js/ChatboxDisplay.js
@@ -9,16 +9,22 @@
  
  ChatboxDisplay.prototype.show = function() {
  	this.$panel.show();
+ 	Helpers.stretchIframeHeight( this.$iframe );
  }
  
  ChatboxDisplay.prototype.init = function() {
+ 	app.mainDisplay.registerStretchables( [ this.$iframe ] );
  }
  
  ChatboxDisplay.prototype.clear = function() {
  	this.openUrl('about:blank')
  }
  
- ChatboxDisplay.prototype.openUrl = function (url) {
+ChatboxDisplay.prototype.openUrl = function (url) {
 	this.$iframe.attr('src', url );
 }
- 
+
+ChatboxDisplay.prototype.setLogin = function(user, password) {
+	window.user = user;
+	window.password = password;
+}
