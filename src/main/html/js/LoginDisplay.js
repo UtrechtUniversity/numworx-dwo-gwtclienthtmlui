1aac50bfec1a783b0e9dd0198e4f4ffa611ac8ab
Velthoven, W.P.G. van (Wim) <w.p.g.vanvelthoven@uu.nl>
2022-10-18 13:34:04 +0200

    https://numworx.atlassian.net/browse/LMS-556
    met hideGuest

--- src/main/html/js/LoginDisplay.js
+++ src/main/html/js/LoginDisplay.js
@@ -19,8 +19,8 @@ function LoginDisplay() {
 	// Bind handlers
 	this.$loginForm.on('submit', $.proxy(this.submitLoginForm,this));
 	var loginGuestBtn = this.loginForm.elements["guestlogin"];
-	
-	$(loginGuestBtn).on('click', $.proxy(this.submitLoginGuest, this));
+	this.$loginGuestBtn = $(loginGuestBtn);
+	this.$loginGuestBtn.on('click', $.proxy(this.submitLoginGuest, this));
 	
 	$("#loginLinks").find("a").on('click', $.proxy(this.clickHyperlink, this));
 	
@@ -86,10 +86,13 @@ LoginDisplay.prototype.showWarning = function(msg) {
 	this.$warningBox.show();
 	this.enable();
 }
-LoginDisplay.prototype.hideMsgBox = function(msg) {
+LoginDisplay.prototype.hideMsgBox = function() {
 	this.$messageBox.hide();
 }
 
+LoginDisplay.prototype.hideGuest = function() {
+	this.$loginGuestBtn.hide();
+}
 /*
  * RETURN FUNCTIONS
  */
