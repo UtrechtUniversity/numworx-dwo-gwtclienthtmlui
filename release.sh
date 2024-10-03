cd658da17d4e34ef5f652b03b8d328072af66dad
Wim van Velthoven <w.p.g.vanvelthoven@uu.nl>
2024-10-03 14:10:00 +0200

    prepare release

--- release.sh
+++ release.sh
@@ -1,1 +1,7 @@
-git checkout master; git pull; git checkout teuniz; mvn -P gitflow jgitflow:release-start; mvn -P gitflow jgitflow:release-finish; git push --tags; git push --all
+#!/bin/sh
+set -e
+git checkout master; git pull; 
+git checkout teuniz; git pull;
+mvn -P gitflow jgitflow:release-start;
+mvn -P gitflow jgitflow:release-finish;
+git push --tags; git push --all
